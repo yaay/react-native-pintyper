0e610ed5bda70d899d2896565c427562198d4df1
Yehia Nabil <yehn@outlook.com>
2024-02-13 16:46:11 +0200

    fix: minor fixes

--- README.md
+++ README.md
@@ -10,13 +10,44 @@ npm install react-native-pin-input
 
 ## Usage
 
-```js
-import { multiply } from 'react-native-pin-input';
+```jsx
+import React, { useState } from 'react';
+import PinInput from 'react-native-pin-input';
 
-// ...
+const YourComponent = () => {
+  const handlePinEntered = (pin) => {
+    console.log('PIN entered:', pin);
+    // Your logic for handling the entered PIN
+  };
 
-const result = await multiply(3, 7);
+  return (
+    <PinInput
+      onPinEntered={handlePinEntered}
+      numberOfDigits={5}
+      containerProps={{ /* Additional View props */ }}
+      containerStyle={{ /* Additional styles for the container */ }}
+      inputProps={{ /* Additional TextInput props */ }}
+      inputStyle={{ /* Additional styles for the TextInput */ }}
+    />
+  );
+};
+
+export default YourComponent;
 ```
+
+## Props
+
+- **`onPinEntered`** (required): Callback function triggered when the user enters the complete PIN. Receives the entered PIN as a string.
+
+- **`numberOfDigits`** (optional, default: `5`): The number of digit boxes in the PIN input.
+
+- **`containerProps`** (optional): Additional props to be passed to the outer `View` container.
+
+- **`containerStyle`** (optional): Additional styles for the outer `View` container.
+
+- **`inputProps`** (optional): Additional props to be passed to each `TextInput` component.
+
+- **`inputStyle`** (optional): Additional styles for each `TextInput` component.
 
 ## Contributing
 
--- src/components/PinInput.tsx
+++ src/components/PinInput.tsx
@@ -1,13 +1,45 @@
-import React, { useState, useRef } from 'react';
-import { View, TextInput, StyleSheet } from 'react-native';
+import React, { useState, useRef, useEffect } from 'react';
+import {
+  View,
+  TextInput,
+  StyleSheet,
+  type TextInputProps,
+  type ViewProps,
+  type StyleProp,
+  type ViewStyle,
+  type TextStyle,
+} from 'react-native';
 
 interface PinInputProps {
   onPinEntered: (pin: string) => void;
+  numberOfDigits?: number;
+  inputProps?: Omit<TextInputProps, 'style'>;
+  inputStyle?: StyleProp<TextStyle>;
+  containerProps?: Omit<ViewProps, 'style'>;
+  containerStyle?: StyleProp<ViewStyle>;
 }
 
-const PinInput: React.FC<PinInputProps> = ({ onPinEntered }) => {
-  const [pin, setPin] = useState(['', '', '', '', '']);
-  const inputRefs = useRef<Array<TextInput | null>>(Array(5).fill(null));
+const PinInput: React.FC<PinInputProps> = ({
+  containerProps = {},
+  containerStyle,
+  inputProps = {},
+  inputStyle,
+  onPinEntered,
+  numberOfDigits = 5,
+}) => {
+  const [pin, setPin] = useState(Array(numberOfDigits).fill(''));
+  const inputRefs = useRef<Array<TextInput | null>>(
+    Array(numberOfDigits).fill(null)
+  );
+
+  useEffect(() => {
+    setPin(Array(numberOfDigits).fill(''));
+    inputRefs.current = Array(numberOfDigits).fill(null);
+  }, [numberOfDigits]);
+
+  useEffect(() => {
+    inputRefs.current[0]?.focus();
+  }, []);
 
   const handlePinChange = (index: number, input: string) => {
     if (input.length <= 1) {
@@ -15,48 +47,47 @@ const PinInput: React.FC<PinInputProps> = ({ onPinEntered }) => {
       newPin[index] = input;
       setPin(newPin);
 
-      if (input.length === 1 && index > 0) {
-        inputRefs.current[index - 1]?.focus();
-      } else if (input.length === 0 && index < 4) {
+      if (input.length === 1 && index < numberOfDigits - 1) {
         inputRefs.current[index + 1]?.focus();
+      } else if (input.length === 0 && index > 0) {
+        inputRefs.current[index - 1]?.focus();
+      } else if (index === 0 && input.length === 0) {
+        inputRefs.current[index]?.focus();
       }
 
       if (newPin.every((digit) => digit !== '')) {
-        onPinEntered(newPin.reverse().join(''));
+        console.log(newPin.join(''));
+        onPinEntered(newPin.join(''));
       }
     }
   };
 
   return (
-    <View style={styles.container}>
-      <View style={styles.pinContainer}>
-        {pin.map((digit, index) => (
-          <TextInput
-            key={index}
-            ref={(ref) => (inputRefs.current[index] = ref)}
-            style={styles.pinBox}
-            keyboardType="numeric"
-            maxLength={1}
-            onChangeText={(text) => handlePinChange(index, text)}
-            value={digit}
-            secureTextEntry
-            onFocus={() => inputRefs.current[index]?.focus()}
-          />
-        ))}
-      </View>
+    <View {...containerProps} style={[containerStyle, styles.pinContainer]}>
+      {pin.map((digit, index) => (
+        <TextInput
+          {...inputProps}
+          key={index}
+          ref={(ref) => (inputRefs.current[index] = ref)}
+          style={[inputStyle, styles.pinBox]}
+          keyboardType="numeric"
+          maxLength={1}
+          onChangeText={(text) => handlePinChange(index, text)}
+          value={digit}
+          cursorColor={'grey'}
+          secureTextEntry
+          onFocus={() => inputRefs.current[index]?.focus()}
+        />
+      ))}
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
   pinContainer: {
     flexDirection: 'row',
     marginBottom: 20,
-    marginTop: 20
+    marginTop: 20,
   },
   pinBox: {
     width: 40,
